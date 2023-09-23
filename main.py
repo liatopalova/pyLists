ba2db8d1a294021a60ef3f21e58533ef90dcb81b
Lia Topalova <lia.topalova@gmail.com>
2023-09-23 16:17:57 +0300

    Implemented third task

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-#задача 1
+# #задача 1
+#
+# line = input("Enter a line please: ")
+# letter_count = 0
+# for char in line:
+#     if char.isalpha():
+#         letter_count += 1
+# print(f"Number of letters in line: {letter_count}")
+# digit_count = 0
+# for char in line:
+#     if char.isdigit():
+#         digit_count += 1
+# print(f"Number of digits in line: {digit_count}")
+#
+# #задача 2
+#
+# string = input("Enter a line please: ")
+# letter = input("Enter a symbol:")
+# symbol_count = string.count(letter)
+# print(f"Number of symbols in string: {symbol_count}")
 
-line = input("Enter a line please: ")
-letter_count = 0
-for char in line:
-    if char.isalpha():
-        letter_count += 1
-print(f"Number of letters in line: {letter_count}")
-digit_count = 0
-for char in line:
-    if char.isdigit():
-        digit_count += 1
-print(f"Number of digits in line: {digit_count}")
+#задача 3
 
-#задача 2
-
-string = input("Enter a line please: ")
-letter = input("Enter a symbol:")
-symbol_count = string.count(letter)
-print(f"Number of symbols in string: {symbol_count}")
+text = input("Enter text: ")
+word = input("Enter word: ")
+if word in text:
+    repl = input("Enter word for replace: ")
+    repl_text = text.replace(word, repl)
+    print(f"New text: {repl_text}")
+else:
+    print("Error!")
