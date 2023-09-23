c2135237c919daa0123660e6e363f47cf13dc7c2
Lia Topalova <lia.topalova@gmail.com>
2023-09-23 15:18:25 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+#задача 1
+
+line = input("Enter a line please: ")
+letter_count = 0
+for char in line:
+    if char.isalpha():
+        letter_count += 1
+print(f"Number of letters in line: {letter_count}")
+digit_count = 0
+for char in line:
+    if char.isdigit():
+        digit_count += 1
+print(f"Number of digits in line: {digit_count}")
+
 
